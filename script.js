e3cc3964e7999129922626b10081c8bd335abf3a
Jasmine-Lapierre <89647613+Jasmine-Lapierre@users.noreply.github.com>
2023-11-15 14:38:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ let buttonCouleurNoir;
 let buttonCouleurRouge;
 let couleur = "255,255,255"
 function setup() {
+  pixelDensity(1);
   createCanvas(windowWidth, windowHeight);
   background(255);
   fill(0,0,0);
